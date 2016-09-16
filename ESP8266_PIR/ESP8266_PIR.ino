55e5ecb27630233e3435472c4631d3de625dee5c
Russell Grokett <rgrokett@yahoo.com>
2016-09-16 18:13:19 -0400

    version 1.2 added power saving to wifi

--- ESP8266_PIR/ESP8266_PIR.ino
+++ ESP8266_PIR/ESP8266_PIR.ino
@@ -2,8 +2,12 @@
  *  ESP8266_PIR 
  *  Motion Sensor using Adafruit HUZZAH ESP8266 and PIR Sensor
  *  Sends events to IFTTT for EMAIL or SMS script
+ *  Project Documentation & files at 
+ *  https://github.com/rgrokett/ESP8266_PIR
  *  
- *  Requires HTTPS SSL.  To get SHA1 Fingerprint:
+ *  IFTTT requires HTTPS SSL. You can elect to verify their cert or just accept it by setting verifyCert variable.
+ *  Default is "false" for initial use.
+ *  If needed, update IFTTT SHA1 Fingerprint used for cert verification:
  *  $ openssl s_client -servername maker.ifttt.com -connect maker.ifttt.com:443 | openssl x509 -fingerprint -noout
  *  (Replace colons with spaces in result)
  *  
@@ -13,8 +17,12 @@
  *  https://github.com/electronicsguy/ESP8266/tree/master/HTTPSRedirect
  *  
  *  The ESP8266WiFi library should include WiFiClientSecure already in it
+ *  
+ *  This version also contains power saving features, turning off Wifi between detects
+ *  Note that some 5V batteries turn themselves off if no load. 
+ *  Set "FiveVoltBat" to true if needed.
  *
- *  version 1.0 2016-06-27 R.Grokett
+ *  version 1.2 2016-09-16 R.Grokett
  */
 
 #include <ESP8266WiFi.h>
@@ -31,28 +39,31 @@ extern "C" {
 
 const char* ssid     = "{YOUR_WIFI_SSID}";  // Your WiFi SSID
 const char* password = "{YOUR_WIFI_PWD}";   // Your WiFi Password
+const char* api_key  = "{YOUR_IFTTT_API_KEY}"; // Your API KEY from https://ifttt.com/maker
+const char* event    = "pirtrigger";             // Your IFTTT Event Name 
 
 const char* host     = "maker.ifttt.com";
 const int port       = 443;
-const char* api_key  = "{YOUR_IFTTT_API_KEY}"; // Your API KEY from https://ifttt.com/maker
-const char* event    = "pirtrigger";             // Your IFTTT Event Name
 
 const char* SHA1Fingerprint="A9 81 E1 35 B3 7F 81 B9 87 9D 11 DD 48 55 43 2C 8F C3 EC 87";  // See SHA1 comment above 
-bool verifyCert = false; // Select true if you want SSL certificate validation
+bool verifyCert = false; // Select false if you do NOT want SSL certificate validation
 
 
 int LED = 0;          // GPIO 0 (built-in LED)
 int PIRpin = 14;      // GPIO 14 (PIR Sensor)
-int motionState;      // cache for current motion state value
 int MOTION_DELAY = 15; // Delay in seconds between events to keep from flooding IFTTT & emails
+bool POWER_SAVE = true; // Save power by turning off Wifi. 
+bool FiveVoltBat = false; // Set to true if 5V bat automatically turns off.
 
+int loops = 0;        // power save "timer"
+int motionState;      // cache for current motion state value
 
 // INITIALIZE and Connect to WiFi
 void setup() {
   Serial.begin(115200);
   delay(10);
 
-  // prepare Geiger input pin
+  // prepare PIR input pin
   pinMode(PIRpin, INPUT);
   digitalWrite(PIRpin, HIGH);
 
@@ -84,16 +95,15 @@ void setup() {
   blink();
   blink();
   blink(); 
-
-  // Set Auto Modem Sleep to save power
-  wifi_fpm_set_sleep_type(MODEM_SLEEP_T);
-
+  
+  powerSave();  // Turn off Wifi
 }
 
 
 // SEND HTTPS GET TO IFTTT
 void sendEvent()
 {
+    powerUp();  // Turn on Wifi
     Serial.print("connecting to ");
     Serial.println(host);
   
@@ -134,7 +144,9 @@ void sendEvent()
 
     // Keep from flooding IFTTT and emails from quick triggers
     delay(MOTION_DELAY * 1000);
-  
+    
+    powerSave();  // Turn off Wifi
+
 }
 
 // Blink the LED 
@@ -148,33 +160,49 @@ void blink() {
 // POWER DOWN WIFI TO SAVE BATTERY
 void powerSave()
 {
-  Serial.println("going to light sleep / 0xFFFFFFF");
+  if (!POWER_SAVE) { return; }
+  Serial.println("going to modem sleep");
   delay(100);
-  wifi_station_disconnect();
-  wifi_set_opmode(NULL_MODE);
-  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
-  wifi_fpm_open();
-  gpio_pin_wakeup_enable(GPIO_ID_PIN(PIRpin), GPIO_PIN_INTR_LOLEVEL);
-  wifi_fpm_do_sleep(0xFFFFFFF);
+  WiFi.forceSleepBegin(0);
   delay(200);
 }
 
 // POWER UP WIFI TO TRANSMIT
 void powerUp()
 {
-  gpio_pin_wakeup_disable();
-  Serial.println("wake up / 0xFFFFFFF");
-  wifi_fpm_close();
-  wifi_set_opmode(STATION_MODE);
-  wifi_station_connect();
-  //wifi_connect();
-  delay(100);
+  if (!POWER_SAVE) { return; }
+  Serial.println("wake up modem");
+  WiFi.forceSleepWake();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+}
+
+// POWER CHECK THE BATTERY
+// Some 5V batteries turn off automatically
+// This should keep them active
+void powerCheck()
+{
+  if (!FiveVoltBat) { return; }
+  if (loops > 30) // pulse the battery every 15 sec or so.
+  {
+      Serial.println("battery check");
+      WiFi.forceSleepWake();
+      delay(100);
+      WiFi.forceSleepBegin(0);   
+      delay(100); 
+      blink();
+      loops = 0;
+  }
+  loops = loops + 1;
 }
 
 
 // MAIN LOOP
 void loop() {
-    delay(500);
+    delay(500); 
 
     // Read PIR motion sensor
     int newState = digitalRead(PIRpin);
@@ -192,6 +220,8 @@ void loop() {
     }
 
     digitalWrite(LED, 1 - motionState);   // LED light motion indication 
+
+    powerCheck();  // Keeps battery active
 }
 
 
